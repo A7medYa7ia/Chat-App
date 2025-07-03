8bfcc70b5a9dab0b2f76ae97a878a8b95bf219f4
Ahmed Yahia <a7medya7ia123@gmail.com>
2025-07-03 10:04:53 +0300

    Implement update profile picture feature.

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
