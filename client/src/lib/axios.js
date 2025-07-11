25bf48bc87a985d2dca088dc0f0df545c56e5804
Ahmed Yahia <a7medya7ia123@gmail.com>
2025-07-11 20:08:25 +0300

    fix bug.

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.Node_ENV === "development"
+    import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
       : "/api",
   withCredentials: true,
